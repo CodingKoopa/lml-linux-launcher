e1f965dcf97ea08cee55ceb05ed225eaabc6c0ab
Koopa <codingkoopa@gmail.com>
2020-08-30 17:20:02 -0400

    Improve mod launcher argument handling.

--- lucas-simpsons-hit-and-run-mod-launcher.sh
+++ lucas-simpsons-hit-and-run-mod-launcher.sh
@@ -101,19 +101,26 @@ function lml_linux_launcher() {
   # Shift the options over to the mod list.
   shift "$((OPTIND - 1))"
 
-  # Generate "-mod" arguments for the mod launcher from the arguments passed to the end of this
-  # script.
+  local -r ZENITY_COMMON_ARGUMENTS=(
+    --title "Lucas' Simpsons Hit & Run Mod Launcher First Time Initialization"
+    --width 500
+  )
+
+  # Generate arguments for the mod launcher from the arguments passed to the end of this script.
   local -a mod_launcher_arguments
-  for FILE in "$@"; do
-    local FILE_EXTENTION=${FILE##*.}
-    if [[ "$FILE_EXTENTION" = "lmlm" ]]; then
+  for file in "$@"; do
+    local extension=${file##*.}
+    if [[ "$extension" = "lmlm" ]]; then
       mod_launcher_arguments+=("-mod")
       # By defualt, Wine maps the Z drive to "/" on the host filesystem.
-      mod_launcher_arguments+=("Z:$FILE")
-    elif [[ "$FILE_EXTENTION" = "lmlh" ]]; then
+      mod_launcher_arguments+=("Z:$file")
+    elif [[ "$extension" = "lmlh" ]]; then
       mod_launcher_arguments+=("-hack")
       # By defualt, Wine maps the Z drive to "/" on the host filesystem.
-      mod_launcher_arguments+=("Z:$FILE")
+      mod_launcher_arguments+=("Z:$file")
+    else
+      zenity "${ZENITY_COMMON_ARGUMENTS[@]}" --warning --text "File \"$file\" not recognized as a \
+file handled by the mod launcher, ignoring."
     fi
   done
 
@@ -153,11 +160,6 @@ may not be correctly installed."
     if [[ "$force_delete_prefix" = true ]]; then
       rm -rf "$WINEPREFIX"
     fi
-    # Arguments passed to Zenity that are always the same.
-    local -r ZENITY_COMMON_ARGUMENTS=(
-      --title "Lucas' Simpsons Hit & Run Mod Launcher First Time Initialization"
-      --width 500
-    )
     # First time initialization subshell, with progress tracked by Zenity's progress bar.
     (
       echo "# Booting up Wine."
